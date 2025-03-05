d2d7f67433895e3f58b730b9f55b12f92827ed5e
chickenrice <29.logan.eggers@ksd.org>
2025-03-05 22:31:20 +0000

    config randombackground.fld + dustbaackground

---
+++ backgrounds/randombackground.fld/randombackground.js
@@ -0,0 +1,6 @@
+// script.js
+// This example uses particles.js library for a particle effect.
+particlesJS.load('background', 'randombackground.json', function() {
+    console.log('randombackground.js loaded - callback');
+  });
+  
